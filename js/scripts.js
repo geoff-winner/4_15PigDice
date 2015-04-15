25d4798e9064acc99df20e5baf8b73af90314a2e
geoff winner & marc lee <student@epicodus.com>
2015-04-15 15:37:56 -0700

    add score count and hide/show functions for buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 
 
 var roll;
-var score = 0;
+var current_score2 = 0;
+var total_score2 = 0;
 var current_score = 0;
 var total_score = 0;
 
@@ -21,6 +22,20 @@ var diceroll = function() {
         } else {
             current_score = current_score + 6;
         } return current_score;
+
+        if (roll === 1) {
+            current_score2 = 0;
+        } else if (roll === 2) {
+            current_score2 = current_score2 + 2;
+        } else if (roll === 3) {
+            current_score2 = current_score2 + 3;
+        } else if (roll === 4) {
+            current_score2 = current_score2 + 4;
+        } else if (roll === 5) {
+            current_score2 = current_score2 + 5;
+        } else {
+            current_score2 = current_score2 + 6;
+        } return current_score2;
     };
 
 
@@ -32,6 +47,13 @@ $(document).ready(function() {
 
         $('.current_score').text(current_score);
 
+        if (roll === 1) {
+            $('#hold').hide();
+            $('#choice').hide();
+            $('#hold2').show();
+            $('#choice2').show();
+        }
+        
         event.preventDefault();
     });
     $('form#hold').submit(function(event) {
@@ -39,6 +61,37 @@ $(document).ready(function() {
         total_score = total_score + current_score;
         $('.total_score').text(total_score);
         current_score = 0;
+        current_score2 = 0;
         $('.current_score').text(current_score);
+        $('#hold').hide();
+        $('#choice').hide();
+        $('#hold2').show();
+        $('#choice2').show();
+
+    });
+
+    $('form#choice2').submit(function(event) {
+        current_score2 = diceroll();
+
+        $('.current_score2').text(current_score2);
+        if (roll === 1) {
+            $('#hold2').hide();
+            $('#choice2').hide();
+            $('#hold').show();
+            $('#choice').show();
+        }
+        event.preventDefault();
+    });
+    $('form#hold2').submit(function(event) {
+        event.preventDefault();
+        total_score2 = total_score2 + current_score2;
+        $('.total_score2').text(total_score2);
+        current_score = 0;
+        current_score2 = 0;
+        $('.current_score2').text(current_score2);
+        $('#hold2').hide();
+        $('#choice2').hide();
+        $('#hold').show();
+        $('#choice').show();
     });
 });
